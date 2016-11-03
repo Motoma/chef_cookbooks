e0cd795896f0fc24a9bd27e15bf1ab211bf71f65
Christopher Gilbert <motoma@gmail.com>
2016-11-03 15:35:12 -0400

    git

--- mining/recipes/run.rb
+++ mining/recipes/run.rb
@@ -1,14 +1,6 @@
-bash 'deploy miner' do
-  user "motoma"
-  cwd "/home/motoma"
-  code <<-EOH
-    git clone https://github.com/miningpool/nheqminer.git
-    cd nheqminer/cpu_xenocat/Linux/asm
-    sh assemble.sh
-    cd ../../../Linux_cmake/nheqminer_cpu
-    cmake .
-    make
-    screen -md ./nheqminer_cpu -l us-east1.zcash.miningpoolhub.com:20570 -u Motoma.`hostname` -p x t 1
-    exit 0
-  EOH
+git 'checkout nheqminer' do
+  repository 'https://github.com/miningpool/nheqminer.git'
+  action :checkout
+  user 'motoma'
+  destination '/home/motoma/nheqminer'
 end
