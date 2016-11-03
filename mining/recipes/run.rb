25d26504f9fcdb63b734a783e7f5d2aeb87f5749
Christopher Gilbert <motoma@gmail.com>
2016-11-03 15:26:27 -0400

    fuych

--- mining/recipes/run.rb
+++ mining/recipes/run.rb
@@ -1,12 +1,14 @@
 bash 'deploy miner' do
+  user "motoma"
+  cwd "/home/motoma"
   code <<-EOH
-    echo `date -I` > /home/motoma/ran
     git clone https://github.com/miningpool/nheqminer.git
     cd nheqminer/cpu_xenocat/Linux/asm
     sh assemble.sh
     cd ../../../Linux_cmake/nheqminer_cpu
     cmake .
     make
-    screen -md ./nheqminer_cpu -l us-east1.zcash.miningpoolhub.com:20570 -u Motoma.`hostname` -p x -t 1
-    EOH
+    screen -md ./nheqminer_cpu -l us-east1.zcash.miningpoolhub.com:20570 -u Motoma.`hostname` -p x t 1
+    exit 0
+  EOH
 end
