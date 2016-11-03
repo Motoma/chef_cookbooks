749404fe094f5626f5b7fb56fea4d20de4ef92ac
Christopher Gilbert <motoma@gmail.com>
2016-11-03 13:54:49 -0400

    Yah

--- cmake/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'zcash'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures zcash'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
