3b780d03ed1cba24dea178edb77ee0d1d0e32811
Christopher Gilbert <motoma@gmail.com>
2016-11-03 15:20:04 -0400

    yawn

--- mining/metadata.rb
+++ mining/metadata.rb
@@ -3,5 +3,5 @@ maintainer       '@motoma'
 maintainer_email 'motoma@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures mining'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description 'Yawn'
 version          '0.1.1'
